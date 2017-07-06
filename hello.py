4361c8837f495386e97938ed919796cda95c8866
976251185 <976251185@qq.com>
2017-07-06 15:11:37 +0800

    new version

--- hello.py
+++ hello.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello, Flask!</h1>'
-
+@app.route('/user/<id>')
+def user(id):
+    return '<h1>Hello, %s!' % id
 if __name__ == '__main__':
     app.run(debug=True)
