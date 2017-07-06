fb4a14d11b575d9fbb3b2c55b15628ecaf6a1d03
976251185 <976251185@qq.com>
2017-07-06 16:54:33 +0800

    Using mode

--- hello.py
+++ hello.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 @app.route('/')
 def index():
-    return '<h1>Hello, Flask!</h1>'
-@app.route('/user/<id>')
-def user(id):
-    return '<h1>Hello, %s!' % id
+    return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
